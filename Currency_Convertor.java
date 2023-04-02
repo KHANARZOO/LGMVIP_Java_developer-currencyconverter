1e43686a93dc1f23002ea7c04113385870552a74
KHANARZOO <115086467+KHANARZOO@users.noreply.github.com>
2023-04-02 13:11:27 +0530

    Add files via upload

---
+++ Currency_Convertor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.currency_convertor;
+
+/**
+ *
+ * @author hp
+ */
+public class Currency_Convertor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
